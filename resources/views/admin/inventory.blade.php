3c94c71875208baf763a521986a59091f7571a96
John Anthony Pesco <pescojohnanthony@gmail.com>
2025-11-04 01:50:31 +0800

    Refactor inventory layout by uncommenting header and content wrapper for improved structure

--- resources/views/admin/inventory.blade.php
+++ resources/views/admin/inventory.blade.php
@@ -4,8 +4,8 @@
 <x-app-layout>
   <x-admin.sidebar/>
 
-  {{-- <div id="content-wrapper" class="transition-all duration-300 lg:ml-64 md:ml-20">
-    <x-admin.header/> --}}
+  <div id="content-wrapper" class="transition-all duration-300 lg:ml-64 md:ml-20">
+    <x-admin.header/>
     <main id="main-content" class="pt-20 p-4 lg:p-8 min-h-screen">
       <div class="mb-6 pt-16">
         <p class="text-sm text-gray-600 dark:text-gray-400">Home / <span class="text-red-700 dark:text-red-300 font-medium">Inventory</span></p>
